2603efd5a34591300ffd0a736cd21688b5d0a3bd
eintim <tim.horlacher@protonmail.com>
2022-12-05 17:22:48 +0100

    Add pytest to flake

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
                     buildInputs = [
                         pythonPackages.flask
                         pythonPackages.gunicorn
+                        pythonPackages.pytest
                         pkgs.vagrant
                         pkgs.terraform
                     ];
